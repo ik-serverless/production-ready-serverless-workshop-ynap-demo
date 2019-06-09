54c69c3fb60c02cd6efa495bd1c95d66926439e6
Yan Cui <theburningmonk@gmail.com>
2019-06-09 22:11:09 +0100

    style: remove unnecessary whitespace

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -9,13 +9,13 @@ const init = async () => {
   }
 
   process.env.TEST_ROOT = "https://sr73zpk0el.execute-api.us-east-1.amazonaws.com/dev"
-  process.env.restaurants_api      = "https://sr73zpk0el.execute-api.us-east-1.amazonaws.com/dev/restaurants"
-  process.env.restaurants_table    = "restaurants_yancui"
-  process.env.AWS_REGION           = "us-east-1"
+  process.env.restaurants_api = "https://sr73zpk0el.execute-api.us-east-1.amazonaws.com/dev/restaurants"
+  process.env.restaurants_table = "restaurants_yancui"
+  process.env.AWS_REGION = "us-east-1"
   
   const { credentials } = await promisify(awscred.load)()
   
-  process.env.AWS_ACCESS_KEY_ID     = credentials.accessKeyId
+  process.env.AWS_ACCESS_KEY_ID = credentials.accessKeyId
   process.env.AWS_SECRET_ACCESS_KEY = credentials.secretAccessKey
 
   console.log('AWS credential loaded')
