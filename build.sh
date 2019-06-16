55acc1d41158323911cbece014a3e207704a990f
Yan Cui <theburningmonk@gmail.com>
2019-06-16 23:56:11 +0100

    build: use md5sum instead

--- build.sh
+++ build.sh
@@ -18,12 +18,12 @@ elif [ "$1" = "build" ] && [ $# -eq 1 ]; then
   npm ci
   zip -r workshop.zip functions static node_modules
 
-  MD5=$(md5 -q workshop.zip)
+  MD5=$(md5sum -q workshop.zip)
   aws s3 cp workshop.zip s3://ynap-production-ready-serverless-yancui/workshop/$MD5.zip
 elif [ "$1" = "deploy" ] && [ $# -eq 2 ]; then
   STAGE=$2
   
-  MD5=$(md5 -q workshop.zip)
+  MD5=$(md5sum -q workshop.zip)
   cd terraform
   terraform apply --var "my_name=yancui" --var "file_name=$MD5"
 else
