079242841bc94e74f1dc33465fcdfc73515dbeb9
Yan Cui <theburningmonk@gmail.com>
2019-06-16 23:45:40 +0100

    build: separate build and deploy steps

--- build.sh
+++ build.sh
@@ -14,15 +14,16 @@ instruction()
 if [ $# -eq 0 ]; then
   instruction
   exit 1
-elif [ "$1" = "deploy" ] && [ $# -eq 2 ]; then
-  STAGE=$2
-
+elif [ "$1" = "build" ] && [$# -eq 1]; then
   npm ci
   zip -r workshop.zip functions static node_modules
 
   MD5=$(md5 -q workshop.zip)
   aws s3 cp workshop.zip s3://ynap-production-ready-serverless-yancui/workshop/$MD5.zip
+elif [ "$1" = "deploy" ] && [ $# -eq 2 ]; then
+  STAGE=$2
   
+  MD5=$(md5 -q workshop.zip)
   cd terraform
   terraform apply --var "my_name=yancui" --var "file_name=$MD5"
 else
