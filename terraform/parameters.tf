a9bb7b2dd4b0f747a29e098843d180e578ed06cd
Yan Cui <theburningmonk@gmail.com>
2019-06-19 01:36:09 +0100

    feat: added kinesis stream

--- terraform/parameters.tf
+++ terraform/parameters.tf
@@ -1,11 +1,17 @@
 resource "aws_ssm_parameter" "table_name" {
-  name = "/big-mouth-${var.my_name}/dev/table_name"
+  name = "/big-mouth-${var.my_name}/${var.stage}/table_name"
   type = "String"
   value = "${aws_dynamodb_table.restaurants_table.name}"
 }
 
 resource "aws_ssm_parameter" "url" {
-  name = "/big-mouth-${var.my_name}/dev/url"
+  name = "/big-mouth-${var.my_name}/${var.stage}/url"
   type = "String"
   value = "${aws_api_gateway_deployment.api.invoke_url}"
+}
+
+resource "aws_ssm_parameter" "stream_name" {
+  name = "/big-mouth-${var.my_name}/${var.stage}/stream_name"
+  type = "String"
+  value = "${aws_kinesis_stream.orders_stream.name}"
 }
