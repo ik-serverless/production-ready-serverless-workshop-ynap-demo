59153f5a6cb7d3a4dc26f76ee12bbb76764b8ed4
Yan Cui <theburningmonk@gmail.com>
2019-06-19 02:39:41 +0100

    feat: added place-order function

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -6,7 +6,8 @@ resource "aws_api_gateway_deployment" "api" {
   depends_on = [
     "aws_api_gateway_integration.get_index_lambda",
     "aws_api_gateway_integration.get_restaurants_lambda",
-    "aws_api_gateway_integration.search_restaurants_lambda"
+    "aws_api_gateway_integration.search_restaurants_lambda",
+    "aws_api_gateway_integration.place_order_lambda"
   ]
 
   lifecycle {
@@ -109,6 +110,39 @@ resource "aws_lambda_permission" "apigw_search_restaurants" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.search_restaurants.arn}"
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_deployment.api.execution_arn}/*/*"
+}
+
+# PLACE-ORDER
+resource "aws_api_gateway_resource" "place_order" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  path_part   = "orders"
+}
+
+resource "aws_api_gateway_method" "place_order_post" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.place_order.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "place_order_lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  resource_id = "${aws_api_gateway_method.place_order_post.resource_id}"
+  http_method = "${aws_api_gateway_method.place_order_post.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.place_order.invoke_arn}"
+}
+
+resource "aws_lambda_permission" "apigw_place_order" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.place_order.arn}"
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_api_gateway_deployment.api.execution_arn}/*/*"
---
+++ terraform/place-order.tf
@@ -0,0 +1,65 @@
+resource "aws_lambda_function" "place_order" {
+  function_name = "${local.function_prefix}-place-order"
+
+  s3_bucket = "${local.deployment_bucket}"
+  s3_key    = "${local.deployment_key}"
+
+  handler = "functions/place-order.handler"
+  runtime = "nodejs8.10"
+
+  role = "${aws_iam_role.place_order_lambda_role.arn}"
+
+  environment {
+    variables = {
+      order_events_stream = "${aws_kinesis_stream.orders_stream.name}"
+    }
+  }
+}
+
+# IAM role which dictates what other AWS services the hello function can access
+resource "aws_iam_role" "place_order_lambda_role" {
+  name = "${local.function_prefix}-place-order-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "place_order_lambda_role_policy" {
+  role       = "${aws_iam_role.place_order_lambda_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy" "place_order_lambda_dynamodb_policy" {
+  name = "place_order_dynamodb_scan"
+  path = "/"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "kinesis:PutRecord",
+      "Resource": "${aws_kinesis_stream.orders_stream.arn}"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "place_order_lambda_dynamodb_policy" {
+  role       = "${aws_iam_role.place_order_lambda_role.name}"
+  policy_arn = "${aws_iam_policy.place_order_lambda_dynamodb_policy.arn}"
+}
